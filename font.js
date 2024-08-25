f6562afbd57c9c8f08d5620401bc1b2d6ff97713
busybarbell <167630345+busybarbell@users.noreply.github.com>
2024-08-24 20:01:40 -0400

    Update font.js

--- font.js
+++ font.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const video = document.getElementById('myVideo');
     const lockedElement = document.getElementById('locked');
+    const firstButton = document.getElementById('first-button');
 
     if (video) {
         console.log('Video element found');
@@ -21,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('10 seconds reached');
             lockedElement.style.cssText = '';
             lockedElement.classList.remove('locked-styled');
+            firstButton.classList.remove('button-changed');
         }
     });
 });
