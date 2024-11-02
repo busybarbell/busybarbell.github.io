32661d2531f816581ac09192d918f08c0ae7c4fd
busybarbell <167630345+busybarbell@users.noreply.github.com>
2024-11-02 06:50:08 -0400

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('first-button');
-
-            button.addEventListener('click', function() {
-                console.log("Button clicked!");
-            });
-        }
-    });
-});
