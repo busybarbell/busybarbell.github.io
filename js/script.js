77db58f6d12281a63d418948a4d0c6331b7ba9d7
busybarbell <167630345+busybarbell@users.noreply.github.com>
2024-11-02 06:48:27 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('first-button');
-    
-    window._wq = window._wq || [];
-    _wq.push({ 
-        id: "odeh9l3bfv",
-        onReady: function(video) {
-            video.bind("timechange", function(t) {
-                if (t >= 30) {
-                    button.disabled = false;
-                }
-            });
 
             button.addEventListener('click', function() {
                 console.log("Button clicked!");
